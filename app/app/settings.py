53a0afd2aed4d4964149c760a4e7028e90b5dfcd
santiagocarod <santiagocaroduque@gmail.com>
2022-07-07 22:30:33 -0500

    Added Postgres Dependencies

--- app/app/settings.py
+++ app/app/settings.py
@@ -78,6 +78,11 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgres',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
