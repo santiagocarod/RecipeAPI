a3865b91faa4ab1e8eb007a287df4cb0c219da0c
santiagocarod <santiagocaroduque@gmail.com>
2022-07-16 21:08:29 -0500

    Create User Model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,24 @@
-from django.db import models  # noqa
+from pyexpat import model
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
