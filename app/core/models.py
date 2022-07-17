85d235cb3054317559eebe14e782637d14ae659e
santiagocarod <santiagocaroduque@gmail.com>
2022-07-16 21:36:46 -0500

    Create User Model

--- app/core/models.py
+++ app/core/models.py
@@ -1,4 +1,3 @@
-from pyexpat import model
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -10,9 +9,20 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('User must have email')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
-        user.save(using=self.db)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -20,5 +30,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
