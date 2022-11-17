771b9a7f415cafc4872c9648a1ebb7e5a5bf8145
santiagocarod <santiagocaroduque@gmail.com>
2022-11-17 17:30:24 -0500

    Section 14 step 97

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
 
-from core.models import Recipe
+from core.models import (
+    Recipe,
+    Tag
+)
 
 
 class RecipeSerializer(serializers.ModelSerializer):
@@ -15,3 +18,11 @@ class RecipeDetailSerializer(RecipeSerializer):
 
     class Meta(RecipeSerializer.Meta):
         fields = RecipeSerializer.Meta.fields + ['description']
+
+
+class TagSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+        read_only_fields = ['id']
